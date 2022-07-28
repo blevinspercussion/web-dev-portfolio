a4197a41a6e7d6c605ec2fd5739e35c82a6ce1a1
Adam Blevins <blevinspercussion@gmail.com>
2022-07-28 08:35:44 -0500

    Added code to populate 'about' section and 'front end projects' section dynamically

--- script.js
+++ script.js
@@ -49,9 +49,11 @@ const mockups = [
     },
 ]
 
-
+// Get main elements
 const mainContent = document.getElementById('main-content');
-
+const aboutLink = document.getElementById('about-link');
+const projectsLink = document.getElementById('projects-link');
+const linksLink = document.getElementById('links-link');
 
 // Create DOM Elements for 'about' section
 
@@ -64,16 +66,24 @@ const aboutImageDiv = document.createElement('div');
 const aboutImage = document.createElement('img');
 
 // Add classes and IDs to 'about' section 
-aboutSection.setAttribute.id('about');
+aboutSection.setAttribute('id', 'about');
 aboutSection.classList.add('about-onscreen');
 aboutImageDiv.classList.add('image');
 aboutImage.classList.add('profile-pic');
 
-// Add content to elements 
+// Add content to 'about' section elements 
 aboutHead.textContent = 'Welcome!';
 aboutSubHead.textContent = 'My name is Adam Blevins and I am a front end web developer.';
 aboutParagraph.textContent = '';
-aboutImage.setAttribute.src = './img/cobra-kai.jpg';
+aboutImage.setAttribute('src', './img/cobra-kai.jpg');
+
+// Append elements 
+aboutSection.appendChild(aboutText);
+aboutText.appendChild(aboutHead);
+aboutText.appendChild(aboutSubHead);
+aboutText.appendChild(aboutParagraph);
+aboutSection.appendChild(aboutImageDiv);
+aboutImageDiv.appendChild(aboutImage);
 
 // Create DOM elements for 'projects' section
 const projectsSection = document.createElement('section');
@@ -83,10 +93,10 @@ const frontEndMockupsLink = document.createElement('li');
 
 
 // Add classes and IDs to 'projects' section
-projectsSection.setAttribute.id = 'projects';
+projectsSection.setAttribute('id', 'projects');
 frontEndProjectsLink.setAttribute = 'front-end-projects-link';
 frontEndProjectsLink.classList.add('sub-link', 'active-link');
-frontEndMockupsLink.setAttribute.id('front-end-mockups-link');
+frontEndMockupsLink.setAttribute('id', 'front-end-mockups-link');
 frontEndMockupsLink.classList.add('sub-link', 'inactive-link');
 
 // Add content to 'projects' section elements
@@ -98,8 +108,19 @@ const frontEndProjects = document.createElement('div');
 const frontEndProjectsText = document.createElement('p');
 const projectCardsDiv = document.createElement('div');
 
+// Add classes and IDs to 'front end projects' section 
+frontEndProjects.setAttribute('id', 'front-end-projects');
+frontEndProjects.classList.add('front-end-projects-onscreen');
+projectCardsDiv.setAttribute('id', 'project-cards');
+
 // Add content to 'front end projects' elements 
 frontEndProjectsText.textContent = 'The projects in this section were designed and coded by me, from conception to completion.'
+
+// Append elements
+projectsSection.appendChild(projectsNav);
+projectsNav.appendChild(frontEndProjectsLink);
+projectsNav.appendChild(frontEndMockupsLink);
+projectsSection.appendChild(projectCardsDiv);
 
 // Create project cards
 for (let project in projects) {
@@ -113,29 +134,34 @@ for (let project in projects) {
 
     // Add classes and IDs 
     projectCardDiv.classList.add('project-card');
-    projectCardDiv.setAttribute.id(project.id);
-    projectCardImage.setAttribute.id('project-image');
+    projectCardDiv.setAttribute('id', projects[project].id);
+    projectCardImage.classList.add('project-image');
     projectCardLinks.classList.add('project-card-links');
 
     // Add content to elements
-    projectCardHeading.textContent = project.title;
-    projectCardImage.setAttribute.src(project.image);
-    projectCardCodeLink.setAttribute.href(project.codeUrl);
-    projectCardCodeLink.setAttribute.target('_blank');
+    projectCardHeading.textContent = projects[project].title;
+    projectCardImage.setAttribute('src', projects[project].image);
+    projectCardCodeLink.setAttribute('href', projects[project].codeUrl);
+    projectCardCodeLink.setAttribute('target', '_blank');
     projectCardCodeLink.textContent = 'github';
-    projectCardDemoLink.setAttribute.href(project.demoUrl);
-    projectCardDemoLink.setAttribute.target('_blank');
+    projectCardDemoLink.setAttribute('href', projects[project].demoUrl);
+    projectCardDemoLink.setAttribute('target', '_blank');
     projectCardDemoLink.textContent = 'live demo';
 
+    // Append elements
+    projectCardsDiv.appendChild(projectCardDiv);
+    projectCardDiv.appendChild(projectCardHeading);
+    projectCardDiv.appendChild(projectCardImage);
+    projectCardDiv.appendChild(projectCardLinks);
+    projectCardLinks.appendChild(projectCardCodeLink);
+    projectCardLinks.appendChild(projectCardDemoLink);
+
 }
 
-addAboutSection = () => {
-
-}
+mainContent.appendChild(aboutSection);
 
 
-
-
+const sectionLinks = document.querySelectorAll('.section-link');
 
 
 
@@ -157,11 +183,20 @@ addAboutSection = () => {
 // const projectsSection = document.getElementById('projects');
 
 // DOM Functions 
-
 removeAboutSection = () => {
+    aboutSection.remove();
+}
+
+hideAboutSection = () => {
     aboutSection.classList.remove('about-onscreen');
     aboutSection.classList.add('about-offscreen');
     aboutSection.style.visibility = 'hidden';
+    aboutSection.addEventListener('transitionend', removeAboutSection());
+    aboutSection.removeEventListener('transitionend', removeAboutSection());
+}
+
+showProjectsSection = () => {
+    mainContent.appendChild(projectsSection);
 }
 
 
@@ -189,23 +224,24 @@ aboutLink.addEventListener('click', () => {
 });
 
 projectsLink.addEventListener('click', () => {
-    removeAboutSection();
+    hideAboutSection();
+    showProjectsSection();
 });
 
-linksLink.addEventListener('click', () => {
-    removeAboutSection();
-});
+// linksLink.addEventListener('click', () => {
+//     removeAboutSection();
+// });
 
-frontEndMockupsLink.addEventListener('click', () => {
-    frontEndProjects.classList.remove('front-end-projects-onscreen');
-    frontEndProjects.classList.add('front-end-projects-offscreen');
-    frontEndProjects.addEventListener('transitionend', () => {
-        frontEndProjects.remove();
-    });
-});
+// frontEndMockupsLink.addEventListener('click', () => {
+//     frontEndProjects.classList.remove('front-end-projects-onscreen');
+//     frontEndProjects.classList.add('front-end-projects-offscreen');
+//     frontEndProjects.addEventListener('transitionend', () => {
+//         frontEndProjects.remove();
+//     });
+// });
 
-frontEndProjectsLink.addEventListener('click', () => {
-    projects.appendChild(frontEndProjects);
-    frontEndProjects.classList.remove('front-end-projects-offscreen');
-    frontEndProjects.classList.add('front-end-projects-onscreen');
-});
+// frontEndProjectsLink.addEventListener('click', () => {
+//     projects.appendChild(frontEndProjects);
+//     frontEndProjects.classList.remove('front-end-projects-offscreen');
+//     frontEndProjects.classList.add('front-end-projects-onscreen');
+// });
